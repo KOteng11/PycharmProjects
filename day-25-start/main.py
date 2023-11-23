20bb83e9fbf1db308122419f0bf87842456af026
KOteng11 <kingsley_oteng@outlook.com>
2023-11-22 20:56:04 -0500

    Added panda library

--- day-25-start/main.py
+++ day-25-start/main.py
@@ -12,3 +12,8 @@
 #         if row[1] != "temp":
 #             temperatures.append(int(row[1]))
 #     print(temperatures)
+
+import pandas
+
+data = pandas.read_csv("weather_data.csv")
+print(data)
