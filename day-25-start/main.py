d5c73c483f0a0999a7a0564e8373ebec0ddee7bf
Kingsley Darko <kingsley_oteng@outlook.com>
2023-12-07 19:39:07 -0500

    Version 6

--- day-25-start/main.py
+++ day-25-start/main.py
@@ -1,30 +1,14 @@
-# with open("weather_data.csv") as file:
-#     data = file.readlines()
-#
-# print(data)
-#
 # import csv
 #
-# with open("weather_data.csv") as file:
-#     data = csv.reader(file)
-#     temperatures: list = []
+# with open("weather_data.csv") as data_file:
+#     data = csv.reader(data_file)
+#     temperatures = []
 #     for row in data:
-#         if row[1] != "temp":
+#         if row[1] != 'temp':
 #             temperatures.append(int(row[1]))
 #     print(temperatures)
 
 import pandas
 
 data = pandas.read_csv("weather_data.csv")
-# print(type(data))
-# print(data["temp"])
-
-data_dict = data.to_dict()
-print(data_dict)
-
-temp_list = data["temp"].to_list()
-print(temp_list)
-
-print(data["temp"].max())
-
-print(data[data.temp == data.temp.max()])
+print(data["temp"])
